8a755de019d8ac99ffd93c1855a4c687ed1d5e82
Kotaro SONODA <kotaro1976@gmail.com>
2022-12-27 18:14:28 +0900

    v0.1.15

--- s3vm_pines/__init__.py
+++ s3vm_pines/__init__.py
@@ -1,2 +1,2 @@
-from .module import train_test_split, train_test_split2, labeled_unlabeled_sample, labeled_unlabeled_test_split, colored_map, xy2idx, zigzag_index, zigzag_sortindices
+from .module import train_test_split, labeled_unlabeled_sample, colored_map, xy2idx, zigzag_index, hilbert_index, scanner_sortindices
 
